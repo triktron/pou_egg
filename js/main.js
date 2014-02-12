5bcc98d3a46c66d4cd9a3f8aea7d3163c84e34e9
triktron <ele.trik@hotmail.com>
2014-02-12 15:23:13 +0100

    3

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
-function print_score() {
-	
+function print_score(tex) {
+	var temp = "";
+	var temp_arr = tex.split("");
+	for (var i = 0;i<temp_arr.length;i++) {
+		temp = temp + "<img src='img/" + temp_arr[i] + ".png'></img>";
+	}
+	document.getElementById("score").innerHTML = temp;
 }
